81ccda9933c62b8e6fc72989869f4e77e049d7f8
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2023-08-04 12:04:27 -0600

    contenido estatico

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express from "express";
+import path from "path";
 const app = express();
 
 const port = 3000;
+const baseURL = path.join(__dirname, "../public");
 
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   res.send("Hello World");
+});
+
+app.get("*", (req, res) => {
+  const url = path.join(baseURL, "404.html");
+  res.sendFile(url);
 });
 
 app.listen(port, () => {
